164264a3cbb4cf34a35e5c175c16299e4f0b9a99
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-31 13:25:31 +0300

    correct6

--- file_book.php
+++ file_book.php
@@ -1,56 +1,57 @@
 <?php
-    if ((!empty($_POST)) && ($_POST["name"] != "") && ($_POST["text"] != "")) {
-        $row = "\n<----->\n" .
-            $_POST["name"] . "\n" .
-            $_POST["email"] . "\n" .
-            $_POST["text"]. "\n" .
-            $timeDate = date("d-m-Y H-i-s");
-        file_put_contents("file_guest.txt", $row, FILE_APPEND);
-        header("Location: ?");
-        die(); 
-    }
-      ?>
-    <!DOCTYPE html>
-    <html lang="en">
+if ((!empty($_POST)) && ($_POST["name"] != "") && ($_POST["text"] != "")) {
+    $row = "\n<----->\n" .
+        $_POST["name"] . "\n" .
+        $_POST["email"] . "\n" .
+        $_POST["text"] . "\n" .
+        $timeDate = date("d-m-Y H-i");
+    file_put_contents("file_guest.txt", $row, FILE_APPEND);
+    header("Location: ?");
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="style265.css">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style265.css">
+</head>
 
-    <body>
+<body>
     <img src="7nSh.gif" alt="">
     <h1>Гостевая книга</h1>
-            <div class="text">
-            
-            <?php
-            date("d.m.Y");
-            $data =file_get_contents("file_guest.txt");
-            $records = explode("<----->", $data);
-            echo "<table border='1'>";
-            foreach ($records as $record) {
-                $row = explode("\n", trim($record));
-                // print_r($row);
-                  echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
-                  } 
-            echo "</table>";
-            ?>
-            <h2><i>Здесь Вы можете оставить свой отзыв</i></h2>
-            </div>
-            <div class="ident">
-           
-                <form action="?" method="POST">
-                    <h2>Введите Ваше имя</h2>
-                    <input type="text" name="name" class="user">
-                    <h2>Ваш e-mail</h2>
-                    <input type="text" name="email" class="mail">
-                    <h2>Ваш отзыв</h2>
-                    <input type="text" name="text" class="comment"><br>
-                    <input type="submit" value="ok" class="input">  
-                </form>
-            </div>
-    </body>
-    </html>
-   
+
+    <div class="text">
+        <?php
+        $data = file_get_contents("file_guest.txt");
+        $records = explode("<----->", $data);
+        echo "<table border='1'>";
+        foreach ($records as $record) {
+            $row = explode("\n", trim($record));
+            // print_r($row);
+            echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
+        }
+        echo "</table>";
+        ?>
+
+    </div>
+    <div class="ident">
+        <h2><i>Здесь Вы можете оставить свой отзыв</i></h2>
+        <form action="?" method="POST">
+            <h2>Введите Ваше имя</h2>
+            <input type="text" name="name" class="user">
+            <h2>Ваш e-mail</h2>
+            <input type="text" name="email" class="mail">
+            <h2>Ваш отзыв</h2>
+            <input type="text" name="text" class="comment"><br>
+            <input type="submit" value="ok" class="input">
+        </form>
+    </div>
+
+
+</body>
+
+</html>
